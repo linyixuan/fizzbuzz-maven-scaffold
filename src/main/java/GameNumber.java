16f291863e53ec7a01b0474354a171ddf384de4d
lynn_pc <linyixuan08@gmail.com>
2020-01-15 19:17:03 +0800

    删除无用代码

--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -1,29 +1,29 @@
 import static java.lang.String.valueOf;
 
 public class GameNumber {
-    private final int number;
+
+    private final int num;
 
     public GameNumber(int i) {
-        this.number = i;
+        this.num = i;
     }
 
     @Override
     public String toString() {
-        String result = null;
-        if (isaBoolean(15)) {
+        String result;
+        if (isaBoolean(3) && isaBoolean(5)) {
             result = "FizzBuzz";
         } else if (isaBoolean(3)) {
             result = "Fizz";
         } else if (isaBoolean(5)) {
             result = "Buzz";
         } else {
-            result = valueOf(number);
-
+            result = valueOf(num);
         }
         return result;
     }
 
     private boolean isaBoolean(int i) {
-        return number % i == 0;
+        return num % i == 0;
     }
 }
---
+++ src/test/java/GameNumberTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameNumberTest {
+    @Test
+    public void cases() {
+        assertEquals(new GameNumber(1).toString(), "1");
+        assertEquals(new GameNumber(3).toString(), "Fizz");
+        assertEquals(new GameNumber(5).toString(), "Buzz");
+        assertEquals(new GameNumber(15).toString(), "FizzBuzz");
+    }
+}
